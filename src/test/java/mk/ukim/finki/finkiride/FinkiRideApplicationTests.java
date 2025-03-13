c58e440a59107e62c18589352c51875d75322c4e
Daniel Ilievski <dilievski54@gmail.com>
2025-03-13 09:32:15 +0100

    Project init

---
+++ src/test/java/mk/ukim/finki/finkiride/FinkiRideApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.finkiride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinkiRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
