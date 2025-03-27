cf2cb64a322cc27f1f6471f1da5eb9783870b3f7
Daniel Ilievski <dilievski54@gmail.com>
2025-03-27 10:31:13 +0100

    Add backend project initial setup

---
+++ wayzi-spring-react/wayzi-backend/src/main/java/mk/ukim/finki/wayzi/WayziApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wayzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WayziApplication.class, args);
+	}
+
+}
---
+++ wayzi-spring-react/wayzi-backend/src/test/java/mk/ukim/finki/wayzi/WayziApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wayzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WayziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
